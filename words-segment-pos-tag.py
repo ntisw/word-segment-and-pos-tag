a3710179fdd94619d0b3634e520e4425e70775e2
Nantipat <scemisnis@gmail.com>
2020-04-22 22:09:06 +0700

    clearing unnecessary print

--- words-segment-pos-tag.py
+++ words-segment-pos-tag.py
@@ -5,89 +5,65 @@ import csv
 
 def read_file(file_name) :
     comments = []
-    with open('./data/'+file_name+'.csv', mode='r', encoding='utf-8') as csv_file:
-        print(f'Read file {file_name}')
+    path_file = f'./data/{file_name}.csv'
+    with open(path_file, mode='r', encoding='utf-8') as csv_file:
+        print(f'Read file {path_file}')
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             comments.append(row['th'])
-    return comments
+    return {"file_name":file_name,"comments":comments}
 
 
-def word_segment(comments) : #using tltk word_segment
+def word_segment(data) : #using tltk word_segment
     results = []
+    file_name = data["file_name"]
+    comments = data["comments"]
     max = len(comments)
-    bar = IncrementalBar('Word segment by tltk',max = max ,suffix='%(percent)d%% %(elapsed_td)s')
+    bar = IncrementalBar(f'Word segment file {file_name}.csv',max = max ,suffix='%(percent)d%% %(elapsed_td)s')
     for row in comments:
-        
         comment = tltk.nlp.word_segment(row)
-        
         comment = comment.replace(">",">|")
         arr = comment.split('|')
         sentences = []
         sentence = []
         for word in arr : 
-            
             if(word.find('<s/>')!= -1) :
-                #sentence.append(word)
                 sentences.append(sentence)
                 sentence = []
             else :
                 sentence.append(word)
-        
-        bar.next()
         results.append(sentences)
+        bar.next()
     bar.finish()
-    
     return results
 
 def word_postag(comments) :
     results = []
-
-    max = len(comments)
-    print('Word postag by pythai . . .',end=" ")
-
     for sentences in comments :
         pos_tag = pos_tag_sents(sentences, corpus='pud')
         results.append(pos_tag)
-    print("Done!")
+    print('Word postag by pythai Done!')
     return results
 
-def write_file(path_file,field,rows) : 
-    # writing to csv file  
-    print(f"write file {path_file}")
-
-    with open(path_file, 'w',newline='',encoding='utf-8') as csvfile:  
-        # creating a csv writer object  
-        csvwriter = csv.writer(csvfile)  
-        # writing the fields  
-        csvwriter.writerow(field)             
-        for row in rows :
-            # writing the data rows  
-            csvwriter.writerow(row) 
- 
-
 def write_file_dict(filename,result):
-    print(f"write file ./result/result_{filename}")
- 
-    with open(f'./result/result_{filename}.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+    
+    path_file = f'./result/result_{filename}.csv'
+    with open(path_file, mode='w', newline='', encoding='utf-8') as csvfile:
         fieldnames = [filename]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in result : 
             writer.writerow({filename:row})
+    print(f"Write Result file {path_file} Done!")
 
 
-def merge_files_result():
-    print(f"Starting... merge files ./result/result_v1")
-    files_name = ["patong_google","promthep_google","wat_google",\
-        "patong_trip","promthep_trip","wat_trip"]
+def merge_files_result(files_name):
     results = []
-    
     for file_name in files_name : 
         result = read_result_file(file_name)
         results.append(result)
-
-    with open(f'./result/result_v1.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+    path_file = f'./result/merged_results.csv'
+    with open(path_file, mode='w', newline='', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=files_name)
         writer.writeheader()
         i=0
@@ -97,25 +73,29 @@ def merge_files_result():
                 files_name[3]:results[3][i],files_name[4]:results[4][i],\
                 files_name[5]:results[5][i]})
             i+=1
+    print(f"Merge files to {path_file} Done!")
 
 def read_result_file(file_name) : 
     result = []    
-
-    with open('./result/result_'+file_name+'.csv', mode='r', encoding='utf-8') as csv_file:
+    path_file = f'./result/result_{file_name}.csv'
+    with open(path_file, mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             result.append(row[file_name])
-    max = len(result)
+    print(f'Read file {path_file} Done!')
     return result
+
+def tltk_segment_pythai_postag(file_name) : 
+    data = read_file(file_name)
+    word_segment_result = word_segment(data)
+    word_postag_result = word_postag(word_segment_result)
+    write_file_dict(file_name,word_postag_result)
 
 def main () :
     files_name= ["patong_google","promthep_google","wat_google"\
         ,"patong_trip","promthep_trip","wat_trip"]
     for file_name in files_name :
-        comments = read_file(file_name)
-        comments = word_segment(comments)
-        result = word_postag(comments)
-        write_file_dict(file_name,result)
-    merge_files_result()
+        tltk_segment_pythai_postag(file_name)
+    merge_files_result(files_name)
 
 main()
